87ad1d239ffb51627078491e5e381db1e27ea517
Britni <bcanale@stuy.edu>
2017-12-13 20:25:23 -0500

    Fixed and Completed TemperatureWindow

--- 09GUI/TemperatureWindow.java
+++ 09GUI/TemperatureWindow.java
@@ -7,35 +7,52 @@ public class TemperatureWindow extends JFrame implements ActionListener{
     private JTextField t;
     private Container pane;
 
-    public TemperatureWindow(){
-	this.setTitle("Temperature Window");
-	this.setSize(600,400);
-	this.setLocation(100,100);
-	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-
-	pane = this.getContentPane();
-	pane.setLayout(new FlowLayout());
-
-	FtoC = new JButton("Convert F to C");
-	CtoF = new JButton("Convert C to F");
-
-	t = new JTextField(5);
-
-	pane.add(FtoC);
-	pane.add(CtoF);
-	pane.add(t);
-    }
-
-
     public void actionPerformed(ActionEvent e){
-	String event = e.getActionCommand();
-	if(event.equals("Convert F to C")){
-	    t.setText("" +( ((Integer.parseInt(t.getText()) - 32)* 5)/9));
+	String s = e.getActionCommand();
+	if(s.equals("Convert F to C")){
+	    try{
+	    t.setText("" +( ((Double.parseDouble(t.getText()) - 32)* 5)/9));
+	    }
+	    catch(NumberFormatException f){
+		t.setText("Illegal imput: Enter a number");
+	    }
 	}
-	if(event.equals("Convert C to F")){
-	    t.setText(""+ (Integer.parseInt(t.getText())*9 / 5 + 32));
+	if(s.equals("Convert C to F")){
+	    try{
+		t.setText(""+ (Double.parseDouble(t.getText())*9 / 5 + 32));
+	    }
+	    catch(NumberFormatException f){
+		t.setText("Illegal imput: Enter a number");
+	    }
 	}
     }
+	
+
+
+    public TemperatureWindow(){
+        this.setTitle("Temperature Window");
+        this.setSize(600,200);
+        this.setLocation(100,100);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        pane = this.getContentPane();
+        pane.setLayout(new FlowLayout());
+
+        FtoC = new JButton("Convert F to C");
+        CtoF = new JButton("Convert C to F");
+
+        t = new JTextField(12);
+
+	FtoC.addActionListener(this);
+	CtoF.addActionListener(this);
+
+        pane.add(FtoC);
+        pane.add(CtoF);
+        pane.add(t);
+    }
+
+
+
 
     public static void main(String[] args) {
 	TemperatureWindow g = new TemperatureWindow();
